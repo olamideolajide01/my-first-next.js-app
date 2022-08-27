066dd555e72f863e9d3972e0ec544648d730a8dc
olajide olamide <olajideseyi8181@gmail.com>
2022-08-27 09:34:38 +0100

    second commit, changed the project from get photos to get users fron an api

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 export const getStaticProps = async () => {
-  const response = await fetch("https://api.github.com/users?_limit=6");
+  const response = await fetch("https://api.github.com/users");
   const data = await response.json();
 
   return {
@@ -21,18 +21,24 @@ const Post = ({ users }) => {
       <div className="grid lg:grid-cols-3 lg:gap-9 gap-6  md:grid-cols-2 grid-cols-1 px-7">
         {users.map((user) => (
           <div key={user.id}>
-            <Link href={"/photos/" + user.id} key={user.id}>
-              <Image
-                className="mt-10 cursor-pointer"
-                src={user.avatar_url}
-                height="200"
-                width="200"
-                alt=""
-              />
-            </Link>
-            <h3 className="bg-slate-200 py-3 my-2  inline-block">
-              {user.login}
-            </h3>
+            <div>
+              <Link href={"/photos/" + user.id} key={user.id}>
+                <Image
+                  className="mt-10 cursor-pointer"
+                  src={user.avatar_url}
+                  height="200"
+                  width="200"
+                  alt=""
+                />
+              </Link>
+            </div>
+            <div>
+              <Link href={"/photos/" + user.id} key={user.id}>
+                <h3 className="bg-slate-200 py-3 cursor-pointer my-2  inline-block">
+                  {user.login}
+                </h3>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
